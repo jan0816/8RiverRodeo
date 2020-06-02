604f79902d07e422deff324364acd8786482f41a
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-06-02 12:55:57 -0600

    Pushing working create, update methods for Fish impls/controller...working on delete now

--- 8RiverRodeo/src/main/java/com/skilldistillery/riverrodeo/controllers/FishController.java
+++ 8RiverRodeo/src/main/java/com/skilldistillery/riverrodeo/controllers/FishController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +58,22 @@ public class FishController {
 			res.setStatus(404);
 		}
 		return createdFish;
+	}
+	@PutMapping("fishes/{fishId}")
+	public Fish updateFish(@PathVariable("fishId") int fishId, @RequestBody Fish fish,
+			HttpServletResponse resp, Principal principal) {
+		try {
+			fish = fishSvc.updateFish(fishId, fish, principal.getName());
+			if (fish == null) {
+				resp.setStatus(400);
+			}else {
+				resp.setStatus(202);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.setStatus(400);
+			fish = null;
+		}
+		return fish;
 	}
 }
