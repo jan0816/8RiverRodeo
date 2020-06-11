81dc07fb2f7405829f1c1ac8ec1ee70ebbd8ba03
sethschn <sethschn301@gmail.com>
2020-06-11 09:17:23 -0600

    Login and register work

--- ngRiverRodeo/src/app/components/register/register.component.ts
+++ ngRiverRodeo/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { Team } from 'src/app/models/team';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newTeam = new Team();
+  newUser1 = new User();
+  newUser2 = new User();
+
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  register(team: Team, user1: User, user2: User){
+    this.newTeam.teamMembers = [];
+    console.log(user1);
+    console.log(user2);
+    team.teamMembers.push(user1);
+    team.teamMembers.push(user2);
+    console.log(team);
+        this.auth.register(team).subscribe(
+          good => {
+            this.auth.login(team.name, team.password).subscribe(
+              great =>{
+                this.router.navigateByUrl("/userLanding");
+                console.log(great);
+              },
+              terrible => {
+                console.error("RegisterComponent.login() Error in login after register");
+                console.error(terrible);
+              }
+            );
+          },
+          bad => {
+            console.error("RegisterComponent.register() method error");
+            console.error(bad);
+          }
+        );
+      }
 
 }
